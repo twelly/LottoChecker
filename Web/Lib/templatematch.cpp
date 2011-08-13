a00e706f52d39c8ed9901d68eda0c29dcb6b22b6
Rolando Tolibas <jjedit@Rolando-Tolibass-iMac.local>
2011-08-12 20:56:55 -0700

    restructure
    restructure code base to fit existing directory structure

--- Web/Lib/templatematch.cpp
+++ Web/Lib/templatematch.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <TemplateMatch.h>
+#include <templatematch.h>
 
 
 // Globals
--- Web/Src/main.cpp
+++ Web/Src/main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <TemplateMatch.h>
+#include <templatematch.h>
 
 
 int main(int argc, char** argv) {
